c96c6a5fc52d2e7f15f97fa7b6b235eb8d3667fa
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-03-27 14:32:28 +0800

    zzy

---
+++ vue工程化/vue-engineer/src/views/Home.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description: 组件
+ * @Author: 海象
+ * @Date: 2021-03-27 13:02:21
+ * @LastEditors: 海象
+ * @LastEditTime: 2021-03-27 13:05:24
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
