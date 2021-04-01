ec02b771d3a0c87eb1ebaa3a421b8f451e47b47d
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-04-01 10:47:09 +0800

    zzy

--- webpack相关知识/webpack01/src/index.js
+++ webpack相关知识/webpack01/src/index.js
@@ -3,7 +3,7 @@
  * @Author: 海象
  * @Date: 2021-04-01 08:23:22
  * @LastEditors: 海象
- * @LastEditTime: 2021-04-01 09:14:03
+ * @LastEditTime: 2021-04-01 10:46:41
  */
 /**
  *  npm  init -y 初始化npm 配置文件
@@ -11,8 +11,20 @@
  *  npm install --save-dev webpack-cli@3.3.12 安装命令行工具
  *  npm i -D webpack@4.44.0 安装指定版本
  *  npx webpack 执行构建
+ * 
+ * webpack 配置核心概念
+ * chunk 指代码块,一个chunk可能由多个模块组成,也用于代码块的分割;
+ * bundle :资源经过webpack 流程解析编译后最终输出的成果文件
+ * Bundle 对应一个chunk 包含一个模块chunk 或多个模块chunk
+ * entry: 入口文件;webpack依赖图谱的起点
+ * output: webpack打包输出的配置
+ * loader:
+ * plugin:
+ * 
   *
+  * 
 */
 import { str } from './a.js'
+
 console.log('hello webpack' + `${str}`);
 console.log(str);
--- webpack相关知识/webpack01/webpack.config.js
+++ webpack相关知识/webpack01/webpack.config.js
@@ -3,7 +3,7 @@
  * @Author: 海象
  * @Date: 2021-04-01 08:56:12
  * @LastEditors: 海象
- * @LastEditTime: 2021-04-01 09:05:39
+ * @LastEditTime: 2021-04-01 10:43:32
  */
 
 const path = require('path')
@@ -11,12 +11,20 @@ const path = require('path')
 // 零配置
 module.exports = {
     // 入口
-    entry: "./src/index.js",
+    // 数组形式单入口,但是有两个chunk
+    entry: ['./src/index.js', "./src/list.js"],
+    // 多入口只能是对象形式
+    // entry: {
+    //     index: "./src/index.js",
+    //     list: "./src/list.js",
+    //     detail: "./src/detail.js"
+    // },
     // 出口
     output: {
         // 绝对路径
         path: path.resolve(__dirname, './dist'),
-        filename: "main.js"
+        // 多入口占位符
+        filename: "[name].js"
     },
     // 打包模式
     mode: "development"
