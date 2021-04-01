5d870b391bd7e374c9cd7cdbaf84d46b9723ced2
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-04-01 08:39:45 +0800

    zzy

---
+++ webpack相关知识/webpack01/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 组件
+ * @Author: 海象
+ * @Date: 2021-04-01 08:23:22
+ * @LastEditors: 海象
+ * @LastEditTime: 2021-04-01 08:28:50
+ */
+/**
+ *  npm  init -y 初始化npm 配置文件
+ *  npm install --save-dev webpack@4.44.0 安装核心库
+ *  npm install --save-dev webpack-cli@3.3.12 安装命令行工具
+ *  npm i -D webpack@4.44.0 安装指定版本
+ *  npx webpack 执行构建
+  *
+*/
+console.log('hello webpack');
