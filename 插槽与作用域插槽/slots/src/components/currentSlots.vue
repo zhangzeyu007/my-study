8353cd783216227e3eb0f60eb330848a4271ab63
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-04-10 10:35:20 +0800

    zzy

--- 插槽与作用域插槽/slots/src/components/currentSlots.vue
+++ 插槽与作用域插槽/slots/src/components/currentSlots.vue
@@ -3,13 +3,15 @@
  * @Author: 海象
  * @Date: 2021-04-10 10:13:03
  * @LastEditors: 海象
- * @LastEditTime: 2021-04-10 10:31:35
+ * @LastEditTime: 2021-04-10 10:34:50
 -->
 <template>
   <div>
+    <!-- 插槽 -->
     <slot :user="user">
       {{ user.name }}
     </slot>
+    <!-- 具名插槽 -->
     <slot name="book" :user="user"></slot>
   </div>
 </template>
@@ -24,8 +26,6 @@ export default {
       },
     };
   },
-  mounted() {},
-  methods: {},
 };
 </script>
 
--- 插槽与作用域插槽/slots/src/views/Home.vue
+++ 插槽与作用域插槽/slots/src/views/Home.vue
@@ -3,12 +3,13 @@
  * @Author: 海象
  * @Date: 2021-04-10 09:59:31
  * @LastEditors: 海象
- * @LastEditTime: 2021-04-10 10:31:52
+ * @LastEditTime: 2021-04-10 10:34:17
 -->
 <template>
   <div class="home">
     <h1>插槽与作用域插槽</h1>
     <current-slots>
+      <!-- 插槽 -->
       <template v-slot:default="slotPrpops">
         {{ slotPrpops.user.age }}
       </template>
