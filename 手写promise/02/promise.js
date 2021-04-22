91a34c3a1667d0ce39baf468a48e3e62b6d4879c
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-04-22 20:21:21 +0800

    zzy

--- 手写promise/02/promise.js
+++ 手写promise/02/promise.js
@@ -3,7 +3,7 @@
  * @Author: 海象
  * @Date: 2021-04-21 17:41:55
  * @LastEditors: 海象
- * @LastEditTime: 2021-04-22 14:11:18
+ * @LastEditTime: 2021-04-22 19:40:31
  */
 
 class zzPromise {
@@ -31,13 +31,13 @@ class zzPromise {
             }, 0)
         }
     }
-    reject(reason) {
+    reject(value) {
         if (this.status === zzPromise.PENDING) {
             this.status = zzPromise.REJECTED
-            this.value = reason;
+            this.value = value;
             setTimeout(() => {
                 this.callbacks.map(callbacks => {
-                    callbacks.onRejected(reason)
+                    callbacks.onRejected(value)
                 })
             }, 0)
         }
@@ -51,22 +51,12 @@ class zzPromise {
         }
         return new zzPromise((resolve, reject) => {
             if (this.status === zzPromise.PENDING) {
-                debugger
                 this.callbacks.push({
                     onFulfilled: value => {
-                        console.log('走啊');
-                        try {
-                            let result = onFulfilled(value);
-                            if (result instanceof zzPromise) {
-                                result.then(resolve, reject)
-                            } else {
-                                resolve(result)
-                            }
-                        } catch (error) {
-                            reject(error)
-                        }
+                        this.parse(onFulfilled(value), resolve, reject)
                     },
                     onRejected: value => {
+                        this.parse(onRejected(value), resolve, reject)
                         try {
                             let result = onRejected(value);
                             if (result instanceof zzPromise) {
@@ -78,39 +68,73 @@ class zzPromise {
                             reject(error)
                         }
                     }
-
                 })
-                console.log(this.callbacks);
             }
             if (this.status === zzPromise.FULFILLED) {
-                console.log('哈哈');
                 setTimeout(() => {
-                    try {
-                        let result = onFulfilled(this.value)
-                        if (result instanceof zzPromise) {
-                            result.then(resolve, reject)
-                        } else {
-                            resolve(result)
-                        }
-                    } catch (error) {
-                        reject(error)
-                    }
+                    this.parse(onFulfilled(this.value), resolve, reject)
                 }, 0)
             }
             if (this.status === zzPromise.REJECTED) {
                 setTimeout(() => {
-                    try {
-                        let result = onRejected(this.value)
-                        if (result instanceof zzPromise) {
-                            result.then(resolve, reject)
-                        } else {
-                            resolve(result)
-                        }
-                    } catch (error) {
-                        reject(error)
-                    }
+                    this.parse(onRejected(this.value), resolve, reject)
                 }, 0)
             }
+        })
+    }
+    parse(result, resolve, reject) {
+        if (promise === result) {
+            throw new TypeError('chaining cycle detected')
+        }
+        try {
+            if (result instanceof zzPromise) {
+                result.then(resolve, reject)
+            } else {
+                resolve(result)
+            }
+        } catch (error) {
+            reject(error)
+        }
+    }
+    static resolve(value) {
+        return new zzPromise((resolve, reject) => {
+            if (value instanceof zzPromise) {
+                value.then(resolve, reject)
+            } else {
+                resolve(value)
+            }
+        })
+    }
+    static reject(reason) {
+        return new zzPromise((resolve, reject) => {
+            reject(reason)
+        })
+    }
+    static all(promises) {
+        let resolves = []
+        return new zzPromise((resolve, reject) => {
+            promises.forEach((promise, index) => {
+                promise.then(
+                    value => {
+                        resolves.push(value)
+                        if (resolves.length == promises.length) {
+                            resolve(resolves)
+                        }
+                    },
+                    reason => {
+                        reject(reason);
+                    }
+                )
+            })
+        })
+    }
+    static race(promises) {
+        return new zzPromise((resolve, reject) => {
+            promises.map(promise => {
+                promise.then(value => {
+                    resolve(value)
+                })
+            })
         })
     }
 }
