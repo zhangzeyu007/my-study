551fd9ffb21d8088121cac5b882d6c7b6080de10
zhangzeyu007 <zhangzeyu1234@gmail.com>
2021-07-19 20:47:04 +0800

    zzy

---
+++ vue生命周期执行顺序/src/components/child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>child</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  beforeCreate () {
+    console.log('beforeCreate child')
+  },
+  created () {
+    console.log('create child')
+  },
+  mounted () {
+    console.log('mounted child')
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ vue生命周期执行顺序/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <child-compent></child-compent>
+  </div>
+</template>
+
+<script>
+import ChildCompent from '../components/child.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ChildCompent
+  },
+  beforeCreate () {
+    console.log('beforeCreate parent')
+  },
+  created () {
+    console.log('created parent')
+  },
+  mounted () {
+    console.log('mounted parent')
+  }
+}
+</script>
