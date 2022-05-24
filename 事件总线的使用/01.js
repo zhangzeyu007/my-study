deb504b910f427be023c61f7fc812bc36e47e183
zhangzeyu007 <zhangzeyu1234@gmail.com>
2022-05-24 19:07:02 +0800

    feat: 更新

--- 事件总线的使用/01.js
+++ 事件总线的使用/01.js
@@ -1,25 +1,26 @@
 /*
- * @Description: 组件
- * @Author: 海象
+ * @Description:
+ * @Author: 张泽雨
  * @Date: 2021-04-15 17:03:04
- * @LastEditors: 海象
- * @LastEditTime: 2021-04-15 17:28:29
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2022-05-24 11:42:51
+ * @FilePath: \my-study\事件总线的使用\01.js
  */
-class Bus {
-    constructor() {
-        this.callbacks = {}
-    }
 
-    $emit(name, fn) {
-        this.callbacks[name] = this.callbacks[name] || []
-        this.callbacks[name].push(fn)
+class Bus {
+  constructor() {
+    this.callbacks = {};
+  }
+
+  $emit(name, fn) {
+    this.callbacks[name] = this.callbacks[name] || [];
+    this.callbacks[name].push(fn);
+  }
+  $on(name, args) {
+    if (this.callbacks[name]) {
+      this.callbacks[name].forEach((cb) => cb(args));
     }
-    $on(name, args) {
-        if (this.callbacks[name]) {
-            this.callbacks[name].forEach(cb => cb(args))
-        }
-    }
-    
+  }
 }
 
-Vue.prototype.$bus = new Bus()
+Vue.prototype.$bus = new Bus();
