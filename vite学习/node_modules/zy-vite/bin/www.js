4c64f564ce887e77d0dac4cac13390cf36613ab7
zhangzeyu007 <zhangzeyu1234@gmail.com>
2022-06-05 21:37:00 +0800

    feat: vite

---
+++ vite学习/node_modules/zy-vite/bin/www.js
@@ -0,0 +1,3 @@
+#! /usr/bin/env node
+// 可以运行的脚本
+console.log("zzy-vite");
--- vite学习/vite-project/src/main.js
+++ vite学习/vite-project/src/main.js
@@ -3,7 +3,7 @@
  * @Author: 张泽雨
  * @Date: 2022-06-05 17:27:32
  * @LastEditors: 张泽雨
- * @LastEditTime: 2022-06-05 19:24:22
+ * @LastEditTime: 2022-06-05 20:16:55
  * @FilePath: \my-study\vite学习\vite-project\src\main.js
  */
 import { createApp } from "vue";
@@ -12,7 +12,7 @@ import App from "./App.vue";
 createApp(App).mount("#app");
 
 // 默认采用的是es6 原生的模块 (import 语法 在 es6 中默认会发送一个请求)
-// 默认会给vue 的模块增加一个前缀 /@moudles
+// 默认会给vue 的模块增加一个前缀 /@module
 // 把 .vue 文件在 后端给解析成一个对象 ( 唯一就是编译了.vue 文件)
 
 // node Koa 快速搭建 http 服务
