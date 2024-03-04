68788628f5566ed8f0a12167e221e537664d7b56
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-04 11:53:35 +0800

    feat: vue router

--- vue生命周期执行顺序/src/views/Home.vue
+++ vue生命周期执行顺序/src/views/Home.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Description:
+ * @Author: 张泽雨
+ * @Date: 2021-07-19 20:16:18
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-04 11:33:41
+ * @FilePath: \my-study\vue生命周期执行顺序\src\views\Home.vue
+-->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
@@ -32,18 +40,23 @@ export default {
     console.log('----parent-----')
     this.$refs.children.go()
   },
+
   beforeUpdate () {
     console.log('beforeUpdate parent')
   },
+
   updated () {
     console.log('update parent')
   },
+
   beforeDestroy () {
     console.log('beforeDestroy parent')
   },
+
   destroyed () {
     console.log('destroyed parent')
   },
+
   methods: {
     goto () {
       console.log('子调用的')
