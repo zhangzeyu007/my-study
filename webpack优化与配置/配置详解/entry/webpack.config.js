09a17245e5cb6f4f6d6f7c02f9a0a34e358309ab
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-09 19:18:56 +0800

    feat: webpack

--- webpack优化与配置/配置详解/entry/webpack.config.js
+++ webpack优化与配置/配置详解/entry/webpack.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Author: 张泽雨
+ * @Date: 2021-07-13 13:19:11
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-09 19:18:14
+ * @FilePath: \my-study\webpack优化与配置\配置详解\entry\webpack.config.js
+ */
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
@@ -6,7 +14,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
         string: 1个 chunk, 1个 bundle (单入口配置)
         arr: 多个入口, 1个 chunk, 1个 bundle (多入口配置)
         JOSN: 有几个 key 就有几个 chunk, 同时也会有几个 bundle
-            key 就是 chunk 的名称 (多入口, 多出口)
+        key 就是 chunk 的名称 (多入口, 多出口)
 
 */
 
