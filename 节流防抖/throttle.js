4b97cfe2f876c83e4f9487c4a26faef7dbc9eb6e
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-13 11:44:08 +0800

    feat: 防抖

--- 节流防抖/throttle.js
+++ 节流防抖/throttle.js
@@ -2,8 +2,8 @@
  * @Description: 组件
  * @Author: 海象
  * @Date: 2021-03-20 14:19:21
- * @LastEditors: 海象
- * @LastEditTime: 2021-03-20 15:29:07
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-13 10:52:48
  */
 // 时间戳节流
 // 第一次执行, 最后一次不会处发函数
@@ -26,38 +26,37 @@
 // }
 
 // 第一次不执行, 最后一次会处发函数 leading:false trailing:true
-// 
+//
 function throttle(func, wait, options) {
-    let context, args, timeout;
-    let old = 0;
-    if (!options) options = {}
-    let later = function () {
-        old = new Date().valueOf()
-        timeout = null
-        func.apply(context, args)
+  let context, args, timeout;
+  let old = 0;
+  if (!options) options = {};
+  let later = function () {
+    old = new Date().valueOf();
+    timeout = null;
+    func.apply(context, args);
+  };
+  return function () {
+    context = this;
+    args = arguments;
+    // 获取当前的时间戳
+    let now = new Date().valueOf();
+    if (options.leading === false && !old) {
+      old = now;
     }
-    return function () {
-        context = this
-        args = arguments
-        // 获取当前的时间戳
-        let now = new Date().valueOf();
-        console.log(options.leading);
-        if (options.leading === false && !old) {
-            old = now
-        }
-        if (now - old > wait) {
-            // 第一次会直接执行
-            if (timeout) {
-                clearTimeout(timeout)
-                timeout = null
-            }
-            // 立即执行
-            func.apply(context, args)
-            old = now;
-            console.log(options.trailing);
-        } else if (!timeout && options.trailing !== false) {
-            // 最后一次也会被执行
-            timeout = setTimeout(later, wait)
-        }
+    if (now - old > wait) {
+      // 第一次会直接执行
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      // 立即执行
+      func.apply(context, args);
+      old = now;
+      console.log(options.trailing);
+    } else if (!timeout && options.trailing !== false) {
+      // 最后一次也会被执行
+      timeout = setTimeout(later, wait);
     }
+  };
 }
