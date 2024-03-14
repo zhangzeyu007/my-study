391d4d947442313d8418e93995af26491673a761
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-14 11:01:10 +0800

    feat: dom

--- 拖拽/drag.js
+++ 拖拽/drag.js
@@ -1,16 +1,20 @@
+/*
+ * @Description:
+ * @Author: 张泽雨
+ * @Date: 2024-03-05 14:36:00
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-14 10:57:47
+ * @FilePath: \my-study\拖拽\drag.js
+ */
 const container = document.querySelector(".container");
 let source;
+// 开始拖拽
 container.addEventListener("dragstart", (e) => {
   console.log(e.target.dataset.effect);
   e.dataTransfer.effectAllowed = e.target.dataset.effect;
   source = e.target;
 });
-
-container.addEventListener("dragover", (e) => {
-  // console.log('dragover',e.target);
-  e.preventDefault();
-});
-
+// 拖拽到目标依次触发
 container.addEventListener("dragenter", (e) => {
   clearBg();
   const dropNode = getDropNode(e.target);
@@ -20,20 +24,25 @@ container.addEventListener("dragenter", (e) => {
   e.preventDefault();
 });
 
+container.addEventListener("dragover", (e) => {
+  // console.log('dragover',e.target);
+  e.preventDefault();
+});
+
 container.addEventListener("drop", (e) => {
   clearBg();
   const dropNode = getDropNode(e.target);
   if (dropNode && dropNode.dataset.drop === e.dataTransfer.effectAllowed) {
-		console.log("可以放置",dropNode.dataset);
+    console.log("可以放置", dropNode.dataset);
     if (dropNode.dataset.drop === "copy") {
       console.log("可以拷贝");
-			dropNode.innerHTML= ''
+      dropNode.innerHTML = "";
       const clone = source.cloneNode(true);
-			clone.dataset.effect = "move"
+      clone.dataset.effect = "move";
       dropNode.appendChild(clone);
     } else {
-			source.remove()
-		}
+      source.remove();
+    }
   }
 });
 
--- 拖拽/js/drag.js
+++ 拖拽/js/drag.js
@@ -1,54 +1,74 @@
 /*
- * @Description: 组件
- * @Author: 海象
+ * @Description:
+ * @Author: 张泽雨
  * @Date: 2021-01-19 12:34:34
- * @LastEditors: 海象
- * @LastEditTime: 2021-04-07 10:11:22
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-14 10:40:44
+ * @FilePath: \my-study\拖拽\js\drag.js
  */
-
-const empty = document.querySelector('div.empty')
-const h1 = document.querySelector('h1')
+const empty = document.querySelector("div.empty");
+const h1 = document.querySelector("h1");
 let username;
 
-document.addEventListener('dragstart', (e) => {
-    e.target.style.border = '5px dashed red'
-    username = e.target.alt
-}, false)
+document.addEventListener(
+  "dragstart",
+  (e) => {
+    e.target.style.border = "5px dashed red";
+    username = e.target.alt;
+  },
+  false
+);
 
-document.addEventListener('drag', (e) => {
-    e.target.style.border = '5px dashed red'
-    empty.style.border = "5px dashed red"
-}, false)
+document.addEventListener(
+  "drag",
+  (e) => {
+    e.target.style.border = "5px dashed red";
+    empty.style.border = "5px dashed red";
+  },
+  false
+);
 
-document.addEventListener('dragend', (e) => {
-    e.target.style.border = 'none'
-    empty.style.border = "none"
+document.addEventListener(
+  "dragend",
+  (e) => {
+    e.target.style.border = "none";
+    empty.style.border = "none";
     // h1.innerHTML = "拖拽你喜欢的动漫"
-    h1.style.color = "black"
-}, false)
+    h1.style.color = "black";
+  },
+  false
+);
 
-empty.addEventListener('dragenter', (e) => {
+empty.addEventListener(
+  "dragenter",
+  (e) => {
     if (empty.firstChild) {
-        empty.removeChild(empty.firstChild)
+      empty.removeChild(empty.firstChild);
     }
-    h1.innerHTML = username
-    e.target.style.color = "red"
-}, false)
+    h1.innerHTML = username;
+    e.target.style.color = "red";
+  },
+  false
+);
 // 拖入进来
-empty.addEventListener("dragover", (e) => {
-    e.preventDefault()
-}, false)
+empty.addEventListener(
+  "dragover",
+  (e) => {
+    e.preventDefault();
+  },
+  false
+);
 // dragleave拖出
 
-empty.addEventListener('drop', (e) => {
-    e.preventDefault()
-    e.target.appendChild(document.querySelector(`img[alt=${username}]`))
-    let domImg = document.querySelector(`img[alt=${username}]`)
-    domImg.style.width = 100 + '%'
-    domImg.style.height = 100 + '%'
-    e.target.style.border = "none"
-}, false)
-
-
-
-
+empty.addEventListener(
+  "drop",
+  (e) => {
+    e.preventDefault();
+    e.target.appendChild(document.querySelector(`img[alt=${username}]`));
+    let domImg = document.querySelector(`img[alt=${username}]`);
+    domImg.style.width = 100 + "%";
+    domImg.style.height = 100 + "%";
+    e.target.style.border = "none";
+  },
+  false
+);
