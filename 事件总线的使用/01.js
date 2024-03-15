7d68e61379d88842e47004eb1633b44f34aae1ad
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-15 11:48:59 +0800

    feat: 事件总线

--- 事件总线的使用/01.js
+++ 事件总线的使用/01.js
@@ -3,21 +3,26 @@
  * @Author: 张泽雨
  * @Date: 2021-04-15 17:03:04
  * @LastEditors: 张泽雨
- * @LastEditTime: 2024-03-14 22:44:09
+ * @LastEditTime: 2024-03-15 11:48:08
  * @FilePath: \my-study\事件总线的使用\01.js
  */
 
 class Bus {
   constructor() {
-    this.callbacks = {};
+    this.events = {};
   }
-  $emit(name, fn) {
-    this.callbacks[name] = this.callbacks[name] || [];
-    this.callbacks[name].push(fn);
+  $on(name, callback) {
+    if (!this.events[name]) {
+      this.events[name] = [];
+    }
+    this.events[name].push(callback);
   }
-  $on(name, args) {
-    if (this.callbacks[name]) {
-      this.callbacks[name].forEach((cb) => cb(args));
+
+  $emit(name, ...args) {
+    if (this.events[name]) {
+      this.events[name].forEach((callback) => {
+        callback(...args);
+      });
     }
   }
 }
