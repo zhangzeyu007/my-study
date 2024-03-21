874f7b86b6e53719a8e4d97c3dfa6c987b74e486
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-03-21 20:53:24 +0800

    feat: 防抖

--- 节流防抖/debounce.js
+++ 节流防抖/debounce.js
@@ -1,31 +1,31 @@
 /*
- * @Description: 组件
- * @Author: 海象
+ * @Description:
+ * @Author: 张泽雨
  * @Date: 2021-03-20 11:21:13
- * @LastEditors: 海象
- * @LastEditTime: 2021-03-20 13:12:43
+ * @LastEditors: 张泽雨
+ * @LastEditTime: 2024-03-21 20:39:42
+ * @FilePath: \my-study\节流防抖\debounce.js
  */
-
 function debounce(func, wait, immediate) {
-    let timeout, result;
-    return function () {
-        // 改变内部this的指向
-        let context = this
-        let args = arguments
-        if (timeout) clearTimeout(timeout)
-        if (immediate) {
-            let callnow = !timeout
-            timeout = setTimeout(() => {
-                timeout = null
-            }, wait)
-            // 立即执行
-            if (callnow) result = func.apply(context, args)
-        } else {
-            timeout = setTimeout(function () {
-                func.apply(context, args)
-            }, wait)
-        }
-
-        return result
+  let timeout, result;
+  return function () {
+    // 改变内部this的指向
+    let context = this;
+    let args = arguments;
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      let callnow = !timeout;
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, wait);
+      // 立即执行
+      if (callnow) result = func.apply(context, args);
+    } else {
+      timeout = setTimeout(function () {
+        func.apply(context, args);
+      }, wait);
     }
+
+    return result;
+  };
 }
