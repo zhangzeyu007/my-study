a1109ccdf16f23b9e26c651057fb45801b33f434
zhangzeyu007 <zhangzeyu1234@gmail.com>
2024-04-12 14:42:35 +0800

    feat: 继承

--- vue3项目/vue3-water/src/views/About.vue
+++ vue3项目/vue3-water/src/views/About.vue
@@ -3,8 +3,8 @@
  * @Author: 张泽雨
  * @Date: 2024-03-06 10:53:40
  * @LastEditors: 张泽雨
- * @LastEditTime: 2024-03-08 10:33:34
- * @FilePath: \vue3-warter\src\views\About.vue
+ * @LastEditTime: 2024-04-11 17:13:06
+ * @FilePath: \my-study\vue3项目\vue3-water\src\views\About.vue
 -->
 <template>
   <div>{{ object.id }} fddsf</div>
@@ -16,77 +16,90 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed, toRefs, toRef, isRef, unref, isProxy } from 'vue'
+import {
+  ref,
+  reactive,
+  computed,
+  toRefs,
+  toRef,
+  isRef,
+  unref,
+  isProxy
+} from "vue";
 
-const map = reactive(new Map([['count', ref(0)]]))
+const map = reactive(new Map([["count", ref(0)]]));
 // 这里需要 .value
-console.log(map)
-console.log(map.get('count').value)
+console.log(map);
+console.log(map.get("count").value);
 
-const object = { id: ref(1) }
-const { id } = object
-console.log(id.value)
+const object = { id: ref(1) };
+const { id } = object;
+console.log(id.value);
 
 const author = reactive({
-  name: 'John Doe',
-  books: ['Vue 2 - Advanced Guide', 'Vue 3 - Basic Guide', 'Vue 4 - The Mystery'],
+  name: "John Doe",
+  books: [
+    "Vue 2 - Advanced Guide",
+    "Vue 3 - Basic Guide",
+    "Vue 4 - The Mystery"
+  ],
   num: 0,
   ff: 1
-})
+});
 
 // 一个计算属性 ref
 const publishedBooksMessage = computed(() => {
-  return author.books.length > 0 ? 'Yes' : 'No'
-})
+  return author.books.length > 0 ? "Yes" : "No";
+});
 
 // 一个计算属性 getter/setter
 const plusOne = computed({
   get: () => {
-    console.log('get')
-    return author.ff
+    console.log("get");
+    return author.ff;
   },
   set: (v) => {
-    console.log('set', v)
-    return (author.ff = v)
+    console.log("set", v);
+    return (author.ff = v);
   }
-})
+});
 // console.log(author.num, '获取')
 // plusOne.value = 10
-console.log(plusOne)
+console.log(plusOne);
 
-const counte = ref(0)
+const counte = ref(0);
 const fff = reactive({
-  name: 'd',
+  name: "d",
   age: 10
-})
-console.log(isRef(counte), 'isRef')
-console.log(unref(counte), 'unref')
-console.log('---------')
-console.log(isRef(fff), 'isRef')
-console.log(unref(fff), 'unref')
-console.log('---------')
-console.log(toRefs(fff))
-console.log(fff.name, fff.age)
-console.log(toRef(fff, 'age'))
-let age = toRef(fff, 'age')
-console.log(age.value)
-console.log('---------')
-console.log(isProxy(fff))
+});
+console.log(isRef(counte), "isRef");
+console.log(unref(counte), "unref");
+console.log("---------");
+console.log(isRef(fff), "isRef");
+console.log(unref(fff), "unref");
+console.log("---------");
+console.log(toRefs(fff));
+console.log(fff.name, fff.age);
+console.log(toRef(fff, "age"));
+let age = toRef(fff, "age");
+console.log(age.value);
+console.log("---------");
+console.log(isProxy(fff));
 const myObject = reactive({
-  title: 'How to do lists in Vue',
-  author: 'Jane Doe',
-  publishedAt: '2016-04-10'
-})
+  title: "How to do lists in Vue",
+  author: "Jane Doe",
+  publishedAt: "2016-04-10"
+});
 // cookie操作
 // 设置 Cookie
 function setCookie(name, value, days) {
-  var expires = ''
+  var expires = "";
   if (days) {
-    var date = new Date()
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-    expires = '; expires=' + date.toUTCString()
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + '=' + (value || '') + expires + '; path=/'
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
-setCookie('name', '张泽雨')
+setCookie("name", "张泽雨");
 </script>
